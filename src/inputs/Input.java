ec9884b660807f545466746560df054635b62bf1
Nicholas <nicholas.cercos@gmail.com>
2025-09-12 03:44:58 -0400

    Changed roll input from keyboard key to mouse button

--- src/inputs/Input.java
+++ src/inputs/Input.java
@@ -12,7 +12,7 @@ public class Input {
 	public static final int UP = KeyEvent.VK_SPACE;
 	public static final int LT = KeyEvent.VK_A;
 	public static final int RT = KeyEvent.VK_D;
-	public static final int ROLL = KeyEvent.VK_R;
+	public static final int ROLL = MouseEvent.BUTTON3;
 	public static final int ATTACK = MouseEvent.BUTTON1;
 	public static final int ESC = KeyEvent.VK_ESCAPE;
 
